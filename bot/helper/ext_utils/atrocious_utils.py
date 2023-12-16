0c34174f75d655d9b163ed40d05378f5b90bea6d
Abdullah Al Noman <101085500+Al-Noman-12@users.noreply.github.com>
2023-12-16 14:19:07 +0600

    Update atrocious_utils.py

--- bot/helper/ext_utils/atrocious_utils.py
+++ bot/helper/ext_utils/atrocious_utils.py
@@ -503,9 +503,6 @@ async def task_utils(message):
     button = None
     user_id = message.from_user.id
     user = await message._client.get_users(user_id)
-    b_msg, button = checking_blacklist(message, button)
-    if b_msg is not None:
-        msg.append(b_msg)
     t_msg = await tasks_limit_check(message)
     if t_msg:
         msg.append(t_msg)
