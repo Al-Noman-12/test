8577a1359ae787d2169996eb6523dfe96a4b90eb
Abdullah Al Noman <101085500+Al-Noman-12@users.noreply.github.com>
2023-12-25 22:53:36 +0600

    Update yt_dlp_download.py

--- bot/helper/mirror_utils/download_utils/yt_dlp_download.py
+++ bot/helper/mirror_utils/download_utils/yt_dlp_download.py
@@ -9,7 +9,9 @@ from bot.helper.telegram_helper.message_utils import sendStatusMessage
 from ..status_utils.yt_dlp_download_status import YtDlpDownloadStatus
 from bot.helper.mirror_utils.status_utils.queue_status import QueueStatus
 from bot.helper.ext_utils.bot_utils import sync_to_async, async_to_sync
-from bot.helper.ext_utils.task_manager import is_queued, stop_duplicate_check
+from bot.helper.ext_utils.task_manager import is_queued
+from bot.helper.ext_utils.atrocious_utils import check_filename, limit_checker, stop_duplicate_check, stop_duplicate_leech
+
 
 LOGGER = getLogger(__name__)
 
