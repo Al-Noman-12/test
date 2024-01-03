104d46772670bfa4505d622b3d42d708cf201566
Abdullah Al Noman <101085500+Al-Noman-12@users.noreply.github.com>
2024-01-03 21:40:17 +0600

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -10,7 +10,7 @@ async def speed_test(client, message):
     download_speed = st.download() / 1_000_000 / 8
     upload_speed = st.upload() / 1_000_000 / 8
     msg = f"Download Speed: {download_speed:.2f} Mbps"
-    msg+ = f"\nUpload Speed: {upload_speed:.2f} Mbps"
+    msg += f"\nUpload Speed: {upload_speed:.2f} Mbps"
     await message.reply(msg)
 
 bot.add_handler(MessageHandler(speed_test, filters=command(f"speedtest{CMD_SUFFIX}")))
